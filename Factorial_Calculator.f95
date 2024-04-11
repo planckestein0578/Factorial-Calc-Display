7c2ea2b9da97563722aaca6fd794653705ace436
Bhaswan G <planckestein0578@gmail.com>
2024-04-11 17:29:07 +0530

    Update Factorial_Calculator.f95

--- Factorial_Calculator.f95
+++ Factorial_Calculator.f95
@@ -4,26 +4,37 @@ program FactorialCalculator
     integer :: number
     integer(kind=8) :: factorial
     integer :: i
+    character(len=1) :: response
     
-    ! Ask the user to enter a number
-    print *, "Enter a number:"
-    read(*,*) number
-    
-    ! Check if the input number is negative
-    if (number < 0) then
-        print *, "Factorial is not defined for negative numbers."
-        stop
-    endif
-    
-    ! Initialize factorial to 1
-    factorial = 1
-    
-    ! Calculate factorial
-    do i = 1, number
-        factorial = factorial * i
+    do
+        ! Ask the user to enter a number
+        print *, "Enter a number:"
+        read(*,*) number
+        
+        ! Check if the input number is negative
+        if (number < 0) then
+            print *, "Factorial is not defined for negative numbers."
+            cycle  ! Continue to the next iteration of the loop
+        endif
+        
+        ! Initialize factorial to 1
+        factorial = 1
+        
+        ! Calculate factorial
+        do i = 1, number
+            factorial = factorial * i
+        end do
+        
+        ! Output the factorial
+        print *, "The factorial of ", number, " is ", factorial
+        
+        ! Ask the user if they want to calculate another factorial
+        print *, "Do you want to calculate the factorial of another number? (y/n)"
+        read(*,*) response
+        
+        ! Check if the user wants to exit
+        if (response /= 'y' .and. response /= 'Y') then
+            exit  ! Exit the loop if the response is not 'y' or 'Y'
+        endif
     end do
-    
-    ! Output the factorial
-    print *, "The factorial of ", number, " is ", factorial
-    
-end program FactorialCalculator
+end
